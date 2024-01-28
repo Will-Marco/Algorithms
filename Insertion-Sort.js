443647447e537b02f39d813f71c7ed66291e3a7c
Will-Marco <mr.marco0101@gmail.com>
2024-01-28 08:51:32 +0500

    add Insertion-Sort

---
+++ Insertion-Sort.js
@@ -0,0 +1,17 @@
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    const currentElement = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > currentElement) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = currentElement;
+  }
+  return array;
+}
+
+const unsortedArray = [1, 5, 9, 7, 2, 8, 0, 6, 4, 3];
+console.log("Unsorted Array:", unsortedArray);
+let sortedArray = insertionSort(unsortedArray);
+console.log("Sorted Array:", sortedArray);
